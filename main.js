77b767f2bb33491e431a28924d3ab1a63a4f9296
1ishanmanisingh@gmail.com <1ishanmanisingh@gmail.com>
2022-09-17 23:07:00 +0530

    PUZZLEDONE

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log('Hello World!');
+
+
+function myFunction() {
+// Get the text field
+
+// Copy the text inside the text field
+ navigator.clipboard.writeText("6F6F6F6F20692069206F69696F206F6F6F206969696F6F6F20696F6F696F20696F6F696F206F6F6F206F6F6F6F206F696F206920696F696F20696969206F696F696F6920696F6F206F20696F6F696F20696F696F2069696920696F2069696F20206F696F206F692069206F6F6F20696F696920696969206F6F6920696F6F206F6F20696F6F206F6F69");
+
+// Alert the copied text
+ alert("YOU REALLY THINK YOU CAN SOLVE IT HAHA ?");
+ alert("and also... you lazy -_- copy it manually if on phone bruh")
+ alert("hey hey seems like u need a hint or maybe not.. ")
+ 
+}
+
+function hint() {
+  
+  navigator.clipboard.writeText("HEHE I CLEARED UR CLIPBOARD BECAUSE U TOOK THE HINT , NOW GO BACK TO PAGE AND COPY AGAIN! LAZY! ");
+  
+  alert("you finally decided to take help hmm now then.. ")
+  alert("hint : arithmòs system")
+  alert("gud luk ;)")
+  
+}
